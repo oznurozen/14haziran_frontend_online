dad49974240bbaa95ffc81ef15691392a8ce4287
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2022-08-10 02:15:26 +0300

    ..
    .

---
+++ 7.Hafta/onyedinciders/deneme.js
@@ -0,0 +1,19 @@
+$("#buton").click(function(){
+    if($("#kare").css("display")=="none"){
+        $("#kare").fadeIn();
+    }
+    else{
+        $("#kare").fadeOut();
+    }
+});
+
+
+
+
+
+
+
+
+// $("#kare").click(function(){
+//     $("#kare").fadeOut();
+// });
